81cfc08fd70be673b78ade116c7875798cc1760e
Tatsuya Oba <toba17893@gmail.com>
2016-10-04 14:50:38 +0900

    not use batch and change loss function to mean squared error

--- Or2.py
+++ Or2.py
@@ -8,30 +8,28 @@ W = tf.Variable(tf.zeros([2, 1]))
 b = tf.Variable(tf.zeros([1]))
 y = tf.tanh(tf.matmul(x, W) + b)
 
-loss = tf.reduce_mean(tf.abs(y - y_))
+mean_squared_error = tf.reduce_mean(tf.square(y - y_))
 optimizer = tf.train.GradientDescentOptimizer(0.5)
-train = optimizer.minimize(loss)
+train = optimizer.minimize(mean_squared_error)
 
 init = tf.initialize_all_variables()
 
 session = tf.Session()
 session.run(init)
 
+def evaluation():
+    for a in range(0, 2):
+        for b in range(0, 2):
+            print "a:%d, b:%d" % (a, b)
+            print session.run(y, feed_dict={x: [[a, b]]})[0][0]
 
 for step in range(0, 100):
-    x_datas = []
-    y_datas = []
-    for batch in range(0, 10):
-        a, b = random.randint(0, 1), random.randint(0, 1)
-        x_datas.append([a, b])
-        y_datas.append([a | b])
-    session.run(train, feed_dict={x: x_datas, y_: y_datas})
-
-#evaluation
-for a in range(0, 2):
-    for b in range(0, 2):
-        print a, b
-        print session.run(y, feed_dict={x: [[a, b]]})
-        print a | b
+    x_data = [random.randint(0, 1), random.randint(0, 1)]
+    y_data = [x_data[0] | x_data[1]]
+    session.run(train, feed_dict={x: [x_data], y_: [y_data]})
+    if step % 10 == 0:
+        print "step:%d" % (step)
+        evaluation()
         print
 
+evaluation()
